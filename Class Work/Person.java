82a9894b9c35a0bfbe6e00af79ad7486fb0e53bf
MikeMiugoWachira <mikemeugo@gmail.com>
2024-07-22 21:22:40 +0300

    Add files via upload

---
+++ Class Work/Person.java
@@ -0,0 +1,43 @@
+/*This program is designed to display a person's details which include the name, age and address */
+
+//Class representing a person
+class Person {
+    // The fields to store the name age and address of the person
+    private String name;
+    private int age;
+    private String address;
+
+    // Constructor to initialize the fields
+    public Person(String name, int age, String address) {
+        this.name = name;
+        this.age = age;
+        this.address = address;
+    }
+
+    // Method to display the details of the person
+    public void displayDetails() {
+        System.out.println("Name:" + name);
+        System.out.println("Age:" + age);
+        System.out.println("Address:" + address);
+    }
+
+    // Method that accepts a person object and calls the printCurrentInstance method
+    public void myDetails(Person person) {
+        person.displayDetails();
+    }
+
+    // Static method to print the current instance details
+    public void printCurrentInstance() {
+        myDetails(this);
+    }
+
+    // Main method to create an object and call a method
+    public static void main(String[] args) {
+        // Creating an object
+        Person Michael = new Person("Michael Miugo Wachira", 20, "500 Nakuru");
+
+        // Calling the printCurrentInstance method
+        Michael.printCurrentInstance();
+    }
+
+}
